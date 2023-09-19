0c63440b151b757810947b951348d7b291da1c1d
LENOVO <sanjida.sultana.anika@gmail.com>
2023-09-19 20:32:48 +0300

    new version

---
+++ module2/main_2_2.py
@@ -0,0 +1,5 @@
+import math
+
+radius = float(input("Input your value:"))
+formula = math.pi*radius*radius
+print("Here's your result:", formula)
---
+++ module2/main_2_3.py
@@ -0,0 +1,6 @@
+length = float(input("What's the length?:"))
+width = float(input("What's the width?:"))
+area = float(length * width)
+print(f"Here is the area:", area)
+perimeter = float(2 * (length + width))
+print(f"Here is the perimeter:", perimeter)
